0da45183877c59417e69c7ea9f30863af7d06f6d
wen.jianhua <wen.jianhua@immomo.com>
2016-12-08 14:14:38 +0800

    http edns

---
+++ controllers/edns.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"fmt"
+
+	"../dns/edns"
+	"github.com/astaxie/beego"
+)
+
+type EDNSController struct {
+	beego.Controller
+}
+
+//type dataModel struct {
+//	Domain    string    `json:"domain"`
+//	Device_ip string    `json:"device_ip"`
+//	Device_sp string    `json:"device_sp"`
+//	DNS       EDNSModel `json:"dns"`
+//}
+
+func (c *EDNSController) Get() {
+	edns.Init()
+	ednsModel := edns.Find(c.GetString("domain"), c.GetString("ip"))
+	fmt.Println(ednsModel)
+	c.Data["json"] = &ednsModel
+	c.ServeJSON()
+}
